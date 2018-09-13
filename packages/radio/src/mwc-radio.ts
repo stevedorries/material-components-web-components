004d95b8e49b32dc498529c00ed82e692e719ac0
Steven Dorries <Steven.Dorries@digitalrisk.com>
2018-09-13 12:42:34 -0400

    >.< The radio button not working right, it was because I forgot that the checked attr doesn't cause CSS rerendering, only changes through the DOM cause it to work as expected.

--- packages/radio/src/mwc-radio.ts
+++ packages/radio/src/mwc-radio.ts
@@ -14,25 +14,27 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { FormElement } from '@material/mwc-base/form-element';
+//import { FormElement } from '@material/mwc-base/form-element';
 import { style } from './mwc-radio-css';
-import { SelectionController } from '@material/mwc-base/selection-controller.js';
+import { RadioGroupController } from './radio-controller';
 import { MDCRadio } from '@material/radio';
-import { LitElement,html } from '@polymer/lit-element';
-
+import { LitElement, html, PropertyValues } from '@polymer/lit-element';
+import { ComponentElement, MDCWebComponentMixin } from '@material/mwc-base/component-element';
+const MWCRadio = MDCWebComponentMixin(MDCRadio);
 export class Radio extends LitElement {
-  value: string;
+  value?: string;
   _boundInputChangeHandler: any;
   _boundInputFocusHandler: any;
   _boundInputBlurHandler: any;
-  _selectionController: any;
-  checked: boolean;
+  _radioGroupController?: RadioGroupController | null = null;  
+  checked: boolean = false;
   name: string;
   disabled: boolean;
   _componentRoot: any;
-  _component: any;
+  _component?:MDCRadio;
+  _boundInputClickHandler: any;
   static get ComponentClass() {
-    return MDCRadio;
+    return MWCRadio;
   }
 
   static get componentSelector() {
@@ -41,7 +43,7 @@ export class Radio extends LitElement {
 
   static get properties() {
     return {
-      checked: { type: Boolean, attribute:true, reflect:true },
+      checked: { type: Boolean, attribute: true, reflect: true },
       disabled: { type: Boolean },
       value: { type: String },
       name: { type: String }
@@ -54,20 +56,21 @@ export class Radio extends LitElement {
     this.disabled = false;
     this.name = '';
     this.value = '';
+    this._boundInputClickHandler = this._inputClickHandler.bind(this);
     this._boundInputChangeHandler = this._inputChangeHandler.bind(this);
     this._boundInputFocusHandler = this._inputFocusHandler.bind(this);
     this._boundInputBlurHandler = this._inputBlurHandler.bind(this);
   }
 
   connectedCallback() {
- //   super.connectedCallback();
-    this._selectionController = SelectionController.getController(this);
-    this._selectionController.register(this);
-    this._selectionController.update(this);
+    super.connectedCallback();
+    this._radioGroupController = RadioGroupController.getController(this);
+    this._radioGroupController!.register(this);
+    this._radioGroupController!.update(this);
   }
 
   disconnectedCallback() {
-  //  this._selectionController.unregister(this);
+    this._radioGroupController!.unregister(this);
   }
 
   renderStyle() {
@@ -78,10 +81,9 @@ export class Radio extends LitElement {
     const { checked, value, name, disabled } = this;
     return html`
        ${this.renderStyle()}
-      <!-- Style should be above this comment -->
-      <div class="mdc-radio ${disabled ? " mdc-radio--disabled " : ""}">
-        <input class="mdc-radio__native-control" ?checked="${checked}" ?disabled="${disabled}" type="radio" name="${name}"
-          @change="${this._boundInputChangeHandler}" @focus="${this._boundInputFocusHandler}" @blur="${this._boundInputBlurHandler}">
+      <div class="mdc-radio ${disabled ? " mdc-radio--disabled " : " "}" @focus="${this._boundInputFocusHandler}" @blur="${this._boundInputBlurHandler}">
+        <input class="mdc-radio__native-control" ?checked="${checked}" ?disabled="${disabled}" type="radio" name="${name}" @click="${this._boundInputClickHandler}"
+          @input="${this._boundInputChangeHandler}">
         <div class="mdc-radio__background">
           <div class="mdc-radio__outer-circle"></div>
           <div class="mdc-radio__inner-circle"></div>
@@ -98,24 +100,45 @@ export class Radio extends LitElement {
       </div>`;*/
   }
 
-  firstUpdated(){
-    this._componentRoot = this.shadowRoot!.querySelector(Radio.componentSelector);
-    this._component = new (Radio.ComponentClass)(this._componentRoot);   
+  updated(changedProperties: PropertyValues) {
+    this.input!.checked = this.checked;
+  }
+  firstUpdated(changedProperties: PropertyValues) {
+    super.firstUpdated(changedProperties);
+    // this._componentRoot = this.shadowRoot!.querySelector(Radio.componentSelector);
+    // this._component = new MDCRadio(this._componentRoot);
+    // this._component.checked = false;
     this.input = this.shadowRoot!.querySelector('input');
   }
-  _inputChangeHandler(e) {
-    console.log(this.id+": checked="+e.target.checked);
-    this._component.checked = e.target.checked;
-    this.checked = this._component.checked;
+  _inputClickHandler(e) {
+    console.log("Clicked " + this.id + ": e=" + e.target.checked + ` this=${this.checked} component=${this._component ? this._component.checked : 'undefined'}`);
+    console.log(e);
+    // this._component.checked = e.target.checked;
+    this.checked = e.target.checked || false;//this._component.checked;
+    this._radioGroupController!.update(this);
   }
-
+  _inputChangeHandler(e) {
+    console.log("Input " + this.id + ": e=" + e.target.checked + ` this=${this.checked} component=${this._component ? this._component.checked : 'undefined'}`);
+    console.log(e);
+    // this._component.checked = e.target.checked || false;
+    // this.checked = e.target.checked || false;//this._component.checked;
+    //this._radioGroupController!.update(this);
+  }
   private input: HTMLInputElement | null = null;
-  _inputFocusHandler(e) {
-    this.input!.focus();
-    this.input!.click();    
+  _inputFocusHandler(e: FocusEvent) {
+    console.log("Focus " + this.id + ": e=" + (<any>e.target!).checked + ` this=${this.checked} component=${this._component ? this._component.checked : 'undefined'}`);
+    console.log(e);
+    if (e.target !== this.input) return;
+    // if(e.relatedTarget instanceof Radio && e.relatedTarget.name == this.name)
+    //   e.relatedTarget.checked = false;
+
+    //this.input!.focus();
+    // this.input!.click();    
   }
 
   _inputBlurHandler(e) {
+    console.log("Blur " + this.id + ": e=" + e.target.checked + ` this=${this.checked} component=${this._component ? this._component.checked : 'undefined'}`);
+    console.log(e);
     this.input!.blur();
   }
 
